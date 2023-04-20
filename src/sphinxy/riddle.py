d0a3d13903d459c197068bb703e913b6f2130ad4
Bradon Zhang <bradonzhang@gmail.com>
2023-04-20 14:40:05 -0600

    Add docstrings

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -4,11 +4,26 @@ from typing import Iterator
 
 @dataclass(frozen=True)
 class Riddle:
+    """Dataclass representing a riddle."""
+
     question: str
     answer: str
 
     def check_answer(self, answer: str) -> bool:
+        """Checks if the answer to the riddle is correct.
+
+        Args:
+            answer (str): The user's answer.
+
+        Returns:
+            bool: Whether the answer to the riddle is correct.
+        """
         return answer.lower() == self.answer.lower()
 
     def get_hint(self) -> Iterator[str]:
+        """Gets a hint iterator.
+
+        Yields:
+            Iterator[str]: A stream of hints for the riddle.
+        """
         yield from iter(self.answer)
--- src/sphinxy/sphinx.py
+++ src/sphinxy/sphinx.py
@@ -2,10 +2,16 @@ from sphinxy.riddle import Riddle
 
 
 class IncorrectAnswer(Exception):
-    ...
+    """Exception for incorrect answer."""
 
 
 class Sphinx:
+    """Class representing a Sphinx, who prompts the user with a riddle.
+
+    Args:
+        name (str): The name of the Sphinx.
+    """
+
     def __init__(self, name: str):
         self._name = name
         self._riddle = Riddle(
@@ -17,16 +23,39 @@ class Sphinx:
         )
 
     def introduce(self) -> str:
+        """Provides the introduction text for the Sphinx.
+
+        Returns:
+            str: The Sphinx's introduction text.
+        """
         return (
             f"Greetings, mortals. I am {self._name}. I have guarded the city of Thebes"
             "for centuries and posed riddles to those who dared to approach me."
         )
 
     def update_riddle(self, riddle: Riddle) -> str:
+        """Updates the riddle the Sphinx asks.
+
+        Args:
+            riddle (Riddle): The updated riddle the Sphinx asks.
+
+        Returns:
+            str: A message from the Sphinx indicating that the riddle has changed.
+        """
         self._riddle = riddle
         return "I have updated my riddle. Are you ready to solve it?"
 
     def pose_riddle(self, include_hint: bool = False) -> tuple[str, str | None]:
+        """Poses the riddle to the user.
+
+        Args:
+            include_hint (bool, optional): Whether to include the question hint.
+            Defaults to False.
+
+        Returns:
+            tuple[str, str | None]: A tuple of the riddle question and its hint (if
+            applicable).
+        """
         hint = (
             f"Hint: The answer starts with the letter '{self._riddle.get_hint()}'."
             if include_hint
@@ -35,6 +64,19 @@ class Sphinx:
         return (self._riddle.question, hint)
 
     def check_riddle_answer(self, answer: str, return_hint: bool = False) -> str:
+        """Evaluates the given answer to the riddle.
+
+        Args:
+            answer (str): The given answer to the riddle.
+            return_hint (bool, optional): Controls whether a hint for the riddle should
+                be returned. Defaults to False.
+
+        Raises:
+            IncorrectAnswer: Exception for incorrect answer.
+
+        Returns:
+            str: The result of the evaluation of the answer.
+        """
         if self._riddle.check_answer(answer):
             return "Your answer was correct. You may pass."
         elif return_hint:
