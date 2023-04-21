c6945839846070cb871c56e9a7290d338182760e
Bradon Zhang <bradonzhang@gmail.com>
2023-04-21 06:47:14 -0600

    Attempt to fix main branch appearing in docs

--- docs/conf.py
+++ docs/conf.py
@@ -46,4 +46,4 @@ html_sidebars = {
 # https://holzhaus.github.io/sphinx-multiversion/master/configuration.html#
 smv_tag_whitelist = r"^v\d+\.\d+\.\d+$"
 smv_branch_whitelist = r"^main$"
-smv_remote_whitelist = r"^main$"
+smv_remote_whitelist = r"^origin$"
